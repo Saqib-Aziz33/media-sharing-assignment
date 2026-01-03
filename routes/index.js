3b480ae21e9d2d3a57bb21dbec877c7a941cc123
Saqib-Aziz33 <saqib.aziz41@gmail.com>
2026-01-03 16:38:48 +0500

    backend routes structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const { notFoundHandler, errorHandler } = require("../services/error.service");
 
 function initRoutes(app) {
-  app.get("/", (req, res) => {
+  app.get("/", (_, res) => {
     res.json({ message: "Welcome to the Media Sharing API" });
   });
+
+  app.use("/auth", require("./auth.routes"));
+  app.use("/users", require("./user.routes"));
+  app.use("/posts", require("./post.routes"));
+
   app.use("/*", notFoundHandler);
   app.use(errorHandler);
 }
